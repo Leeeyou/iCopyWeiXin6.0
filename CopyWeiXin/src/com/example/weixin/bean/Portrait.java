8ed76c89aed83e30a60cc7073eceb396e8b2f551
LeeeYou <kongbeily@qq.com>
2014-10-22 18:13:39 +0800

    bean

---
+++ CopyWeiXin/src/com/example/weixin/bean/Portrait.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.weixin.bean;
+
+public class Portrait {
+
+    public static final String[] sPortraitStrings = {
+            "http://img.my.csdn.net/uploads/201409/09/1410248773_6644.jpg",
+            "http://img.my.csdn.net/uploads/201409/09/1410248774_9379.jpg", 
+            "http://img.my.csdn.net/uploads/201406/25/1403669636_8095.png",
+            "http://img.my.csdn.net/uploads/201405/01/1398957333_2353.jpg"
+    };
+
+}
